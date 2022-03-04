3012119acdc60678d2885e743fd628f92662e070
michaelkimmer500 <michaelkimmer500@gmail.com>
2022-03-03 21:34:31 -0500

    Added a homepage subclass to the testing file

--- unitTests.py
+++ unitTests.py
@@ -15,4 +15,27 @@ class Testing(unittest.TestCase):
         # test if input throws error if list
         assertFalse(verifyInput([1, 2, 3]))
 
+class HomePageTesting(unittest.TestCase):
+#Testing subclass specific to the homepage and its related functions
+
+	#connects to the database
+	def setup(self):
+		self.dbname = "AmIAverage"
+		self.db = sqlite3.connect(self.dbname)
+		self.c = self.db.cursor()
+
+	#since home page will not be altering database, no need to commit any changes to it
+	def teardown(self):
+		self.db.close()
+
+	#unit test to test the get_categories() function
+	def test_get_categories(self):
+
+		#unsure if there is a better way to test, since this is the same query from the function
+		cats = self.c.execute("SELECT name FROM Categories;")
+		i = 0
+
+		for name in get_categories(self.dbname):
+			assertEqual(name, cats[i][0], "Error:\nExpected: {}\nActual: {}".format(cats[i][0], name))
+
 
